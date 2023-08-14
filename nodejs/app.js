86c6726dce947123cac6ef07cf5311dcb9132ab3
Seyeong1020 <135409562+Seyeong1020@users.noreply.github.com>
2023-08-14 17:06:57 +0900

    Delete nodejs directory

--- nodejs/app.js
+++
@@ -1,8 +0,0 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
-
-function onLoginClick(){
-    const username = loginInput.value;
-}
-loginButton.addEventListener("click", onLoginClick);
