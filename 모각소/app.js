818a6a0ed5e61a0625494b5dfe28e23f5e3ece85
Seyeong1020 <135409562+Seyeong1020@users.noreply.github.com>
2023-08-30 17:00:28 +0900

    5차시
    마지막 완료

--- 모각소/app.js
+++ 모각소/app.js
@@ -1,24 +1,22 @@
-const express = require('express')
-const ejs = require('ejs')
-const app = express()
-const port = 8080
-const path = require('path')
-const mysql = require('mysql2')
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require('path');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
 
-var bodyParser = require('body-parser')
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.set('view engine', 'ejs')
-app.set('views', './views')
-
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(express.static(path.join(__dirname,'views')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'views')));
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'ohseyeong1020.',
   database: 'login_data'
-})
+});
 
 connection.connect((err) => {
   if (err) {
@@ -28,20 +26,121 @@ connection.connect((err) => {
   console.log('Connected to MySQL as id ' + connection.threadId);
 });
 
+const connectionForPosts = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'ohseyeong1020.',
+  database: 'posts_data'
+});
+
+connectionForPosts.connect((err) => {
+  if (err) {
+    console.error('MySQL connection error: ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as id ' + connectionForPosts.threadId);
+});
+
 // 라우팅
 app.get('/', (req, res) => {
-  res.render('index') // ./views/index.ejs 거 출력하기
-})
+  res.render('index');
+});
 
 app.get('/login', (req, res) => {
-    res.render('login')
-  })
-  
-app.post('/signup', (req, res) => {
-    const {username, password} = req.body;
+  res.render('login');
+});
 
-    const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
-    connection.query(sql, [username, password], (err, results) => {
+app.get('/menu', (req, res) => {
+  const sql = 'SELECT * FROM text';
+  connectionForPosts.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ' + err.stack);
+      return res.status(500).send('Error');
+    }
+    res.render('menu', { posts: results });
+  });
+});
+
+app.get('/write', (req, res) => {
+  res.render('write');
+});
+
+app.post('/write', (req, res) => {
+  const { title, writer, content } = req.body;
+  const sql = 'INSERT INTO text (title, writer, content) VALUES (?, ?, ?)';
+  connectionForPosts.query(sql, [title, writer, content], (err, results) => {
+    if (err) {
+      console.error('Error creating post: ' + err.stack);
+      res.status(500).send('Error creating post');
+      return;
+    }
+    res.redirect('menu')
+  });
+});
+
+app.get('/posts/:id', (req, res) => {
+  const postId = req.params.id;
+  const sql = 'SELECT * FROM text WHERE id = ?';
+  connectionForPosts.query(sql, [postId], (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ' + err.stack);
+      return res.status(500).send('Error');
+    }
+    res.render('post_detail', { post: results[0] });
+  });
+});
+
+// 글 수정 페이지 표시
+app.get('/posts/:id/edit', (req, res) => {
+  const postId = parseInt(req.params.id, 10);
+  if (isNaN(postId)) {
+    return res.status(400).send('Invalid postId');
+  }
+  console.log('Received postId:', postId);
+  const sql = 'SELECT * FROM text WHERE id = ?';
+  connectionForPosts.query(sql, [postId], (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ' + err.stack);
+      return res.status(500).send('Error');
+    }
+    res.render('edit_post', { post: results[0] });
+  });
+});
+
+// 글 수정 처리
+app.post('/posts/:id/edit', (req, res) => {
+  const postId = parseInt(req.params.id, 10);
+  if (isNaN(postId)) {
+    return res.status(400).send('Invalid postId');
+  }
+  const { title, content } = req.body;
+  const sql = 'UPDATE text SET title = ?, content = ? WHERE id = ?';
+  connectionForPosts.query(sql, [title, content, postId], (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ' + err.stack);
+      return res.status(500).send('Error executing query: ' + err.message);
+    }
+    res.redirect('/posts/' + postId);
+  });
+});
+
+// 글 삭제 처리
+app.post('/posts/:id/delete', (req, res) => {
+  const postId = req.params.id;
+  const sql = 'DELETE FROM text WHERE id = ?';
+  connectionForPosts.query(sql, [postId], (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ' + err.stack);
+      return res.status(500).send('Error');
+    }
+    res.redirect('/menu');
+  });
+});
+
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+  const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
+  connection.query(sql, [username, password], (err, results) => {
     if (err) {
       console.error('Error creating user: ' + err.stack);
       res.status(500).send('Error creating user');
@@ -49,35 +148,32 @@ app.post('/signup', (req, res) => {
     }
     res.send('User created successfully');
   });
-})
+});
 
 app.post('/login', (req, res) => {
-  const { loginUsername, loginPassword } = req.body; // 수정된 필드 이름으로 
-
-  const sql = 'SELECT * FROM users WHERE username = ? AND password = ?'; // 원래대로 SQL 쿼리 작성
-
-  connection.query(sql, [loginUsername, loginPassword], (err, results) => { // 수정된 필드 이름으로 
+  const { loginUsername, loginPassword } = req.body;
+  const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+  connection.query(sql, [loginUsername, loginPassword], (err, results) => {
     if (err) {
       console.error('Error executing MySQL query: ' + err.stack);
       return res.status(500).send('Error');
     }
-
-    if (results.length === 1) {
-      // 로그인 성공
-     // req.session.user = loginUsername; // 수정된 필드 이름으로 
-      res.send('로그인 성공');
+    if (results.length > 0) {
+      res.redirect('/mainpage');
     } else {
-      // 로그인 실패
-      res.status(401).send('로그인 성공');
+      res.status(401).send('로그인 실패');
     }
   });
 });
 
-
 app.get('/mainpage', (req, res) => {
-    res.render('mainpage(logout)')
-})
+  res.render('mainpage(logout)');
+});
+
+app.get('/mypage', (req, res) => {
+  res.render('mypage');
+});
 
 app.listen(port, () => {
-  console.log(`서버가 실행되었습니다 주소 : http://localhost:${port}`)
-})
+  console.log(`서버가 실행되었습니다 주소 : http://localhost:${port}`);
+});
